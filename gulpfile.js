e3ab39f94fd3713ea87c4f57818ce412c417bc23
HenryNguyen5 <ngu.henry@hotmail.com>
2018-04-03 19:41:14 -0400

    Fix published typings

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require('gulp');
+const alias = require('path-alias-resolver/gulp');
+
+gulp.task('default', () => {
+  return gulp
+    .src(['./src/**/*.ts', '!./src/**/*.spec.ts'])
+    .pipe(alias('.', { '@src': './src' }))
+    .pipe(gulp.dest('./dist/lib'));
+});
