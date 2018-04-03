d7fdf4a0f2a4df2eb040d7ffbf1a0a2286235238
HenryNguyen5 <ngu.henry@hotmail.com>
2018-04-03 19:50:39 -0400

    Ignore dist and test files

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const alias = require('path-alias-resolver/gulp');
 
 gulp.task('default', () => {
   return gulp
-    .src(['./src/**/*.ts', '!./src/**/*.spec.ts'])
+    .src(['./src/**/*.ts', '!./src/**/*.spec.ts', '!./src/**/*.test.ts'])
     .pipe(alias('.', { '@src': './src' }))
     .pipe(gulp.dest('./dist/lib'));
 });
