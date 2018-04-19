92ae1eceeb73099a2a163e88492b8a1c639e2b3a
HenryNguyen5 <ngu.henry@hotmail.com>
2018-04-19 15:43:10 -0400

    Wait for web3 methods (#59)
    * Add signmessages to the list of calls that should instantly fail
    
    * Add wait time override for web3 methods

--- __test__/apiTests/web3.spec.ts
+++ __test__/apiTests/web3.spec.ts
@@ -46,4 +46,65 @@ describe('web3 tests', () => {
     expect(signMsgCall.error).toBeTruthy();
     expect(signMsgCall.numOfRetries).toEqual(0);
   });
+
+  it.only(
+    'should wait for longer than configured timeout for web3 methods (5 minutes) to allow for user interaction (like metamask tx confirmation)',
+    async () => {
+      const { shepherd, redux } = getAPI();
+      const node = await shepherd.init({
+        customProviders: { MockProvider: MockProviderImplem },
+        providerCallRetryThreshold: 1,
+      });
+
+      const providerArgs = [
+        new MockProvider(),
+        createMockProxyHandler({
+          baseDelay: 3000,
+          failureRate: 0,
+          getCurrentBlockDelay: 500,
+        }),
+      ];
+
+      shepherd.useProvider(
+        'MockProvider',
+        'eth1',
+        makeMockProviderConfig({
+          concurrency: 4,
+          network: 'ETH',
+          requestFailureThreshold: 2,
+          timeoutThresholdMs: 1000,
+        }),
+        ...providerArgs,
+      );
+
+      await Promise.all([
+        node.sendTransaction({} as any),
+        node.signMessage({} as any, ''),
+      ]);
+
+      const signTxCall = getProviderCallById(redux.store.getState(), 0);
+
+      expect(signTxCall.pending).toEqual(false);
+      expect(signTxCall.error).toBeFalsy();
+      console.log(signTxCall);
+      expect(signTxCall.numOfRetries).toEqual(0);
+
+      const signMsgCall = getProviderCallById(redux.store.getState(), 1);
+
+      expect(signMsgCall.pending).toEqual(false);
+      expect(signMsgCall.error).toBeFalsy();
+      expect(signMsgCall.numOfRetries).toEqual(0);
+
+      try {
+        await node.getBalance('0x');
+      } catch {
+        const getBalanceCall = getProviderCallById(redux.store.getState(), 2);
+
+        expect(getBalanceCall.pending).toEqual(false);
+        expect(getBalanceCall.error).toBeTruthy();
+        expect(getBalanceCall.numOfRetries).toEqual(1);
+      }
+    },
+    70000,
+  );
 });
