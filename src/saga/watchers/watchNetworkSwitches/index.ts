e66606e7b87a14226e27536e1ba1b789893c244e
HenryNguyen5 <ngu.henry@hotmail.com>
2018-04-19 14:22:30 -0400

    Buffer network switches

--- src/saga/watchers/watchNetworkSwitches/index.ts
+++ src/saga/watchers/watchNetworkSwitches/index.ts
@@ -4,26 +4,37 @@ import {
   IBalancerInit,
   IBalancerNetworkSwitchRequested,
 } from '@src/ducks/providerBalancer/balancerConfig';
-import { SagaIterator } from 'redux-saga';
-import { call, fork, put, take } from 'redux-saga/effects';
+import { logger } from '@src/utils/logging';
+import { buffers, SagaIterator } from 'redux-saga';
+import { actionChannel, call, fork, put, take } from 'redux-saga/effects';
 import { initializeNewNetworkProviders } from './helpers';
 
-function* handleNetworkSwitch(): SagaIterator {
-  while (true) {
-    const {
-      payload,
-    }: IBalancerNetworkSwitchRequested | IBalancerInit = yield take([
-      BALANCER.NETWORK_SWTICH_REQUESTED,
-      BALANCER.INIT,
-    ]);
+function* handleNetworkSwitch({
+  payload,
+  meta,
+}: IBalancerNetworkSwitchRequested | IBalancerInit): SagaIterator {
+  const networkSwitchPayload = yield call(
+    initializeNewNetworkProviders,
+    payload.network,
+  );
+  logger.log(`Network switch to ${payload.network} succeeded`);
 
-    const networkSwitchPayload = yield call(
-      initializeNewNetworkProviders,
-      payload.network,
+  yield put(balancerNetworkSwitchSucceeded(networkSwitchPayload, meta.id));
+}
+
+function* networkSwitchActionChannel() {
+  const chan = yield actionChannel(
+    [BALANCER.NETWORK_SWTICH_REQUESTED, BALANCER.INIT],
+    buffers.expanding(50),
+  );
+  while (true) {
+    const action: IBalancerNetworkSwitchRequested | IBalancerInit = yield take(
+      chan,
     );
-    yield put(balancerNetworkSwitchSucceeded(networkSwitchPayload));
+    logger.log(`Taking action ${JSON.stringify(action, null, 1)}`);
+    yield call(handleNetworkSwitch, action);
   }
 }
 
 // we dont use takeevery here to avoid processing two switch requests at the same time
-export const watchNetworkSwitches = [fork(handleNetworkSwitch)];
+export const watchNetworkSwitches = [fork(networkSwitchActionChannel)];
